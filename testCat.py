79435865eb64ad72f04bd3293a0a4537b27ecf5d
irinaam <mironovairina972@gmail.com>
2022-02-24 14:25:58 +0300

    module 16 Task 16_7

---
+++ testCat.py
@@ -0,0 +1,12 @@
+from rectangle import Cat
+
+cat_1 = Cat('Барон', 'мальчик', '2 год')
+cat_2 = Cat('Сэм', 'мальчик', '2 год')
+
+print('имя:', cat_1.name)
+print('пол:', cat_1.gender)
+print('возраст:', cat_1.age)
+
+print('имя:', cat_2.name)
+print('пол:', cat_2.gender)
+print('возраст:', cat_2.age)
